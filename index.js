a7620608664eadd1ff38435cc95bc54025e91102
Anas Abid Shaikh <115411308+anasabidshaikh@users.noreply.github.com>
2022-12-05 19:26:50 +0500

    transpile type

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+// Hello World of TS
 let greetings = "Hello";
 console.log(greetings);
+// Strongly type inference to make the type as described
+let a = "Pakistan";
+a = "USA";
+// type string value can be changed with type string
+let b = 12;
+let c = true;
+// Type Inference, to automatically get type from its values
+let e = "USA";
+console.log(typeof e);
+let f = 10.9;
+console.log(typeof f);
