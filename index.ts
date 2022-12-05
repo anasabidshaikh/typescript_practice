dfb15cfb236a3a2b4862ba7560e7cec43c97c431
Anas Abid Shaikh <115411308+anasabidshaikh@users.noreply.github.com>
2022-12-05 19:26:26 +0500

    type of typescripts

--- index.ts
+++ index.ts
@@ -1,2 +1,16 @@
+// Hello World of TS
 let greetings: String = "Hello";
 console.log(greetings);
+
+// Strongly type inference to make the type as described
+let a: String = "Pakistan";
+a = "USA";
+// type string value can be changed with type string
+let b: Number = 12;
+let c: Boolean = true;
+
+// Type Inference, to automatically get type from its values
+let e = "USA";
+console.log(typeof e);
+let f = 10.9;
+console.log(typeof f);
